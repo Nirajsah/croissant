efa42ccdae9460f704ca5288bd217bcddf3e00b2
Nirajsah <nirajsahy4aj@gmail.com>
2025-04-22 21:25:59 +0530

    feat: update font styles in index.html, enhance manifest description, and add new CSS utilities for responsive design

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import HomePage from './components/HomePage'
 import Welcome from './components/Welcome'
 import React from 'react'
 import './index.css'
-import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import { MemoryRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import WalletProvider from './store/WalletProvider'
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
       <div className="font-russo font-thin text-white w-[357px] h-[600px] flex flex-col items-center justify-center bg-black">
         <MemoryRouter>
           <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="/home" element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/set" element={<Welcome />} />
           </Routes>
         </MemoryRouter>
       </div>
